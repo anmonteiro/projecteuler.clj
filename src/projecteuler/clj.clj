3c5830e8ccc889e09cbb5093e2db202c00be4cc9
António Nuno Monteiro <anmonteiro@gmail.com>
2014-05-25 16:53:19 +0100

    solve 1st problem

--- src/projecteuler/clj.clj
+++ src/projecteuler/clj.clj
@@ -1,6 +1,10 @@
 (ns projecteuler.clj)
 
 (defn problem1
-  "I don't do a whole lot."
+  "Find the sum of multiples of 3 and 5 below x"
   [x]
-  (+ x 13))
+  (if (= x 0)
+  	x
+  	(if (or (= (mod (- x 1) 3) 0) (= (mod (- x 1) 5) 0))
+  		(+ (- x 1) (problem1 (- x 1)))
+  		(+ 0 (problem1 (- x 1))))))
--- test/projecteuler/clj_test.clj
+++ test/projecteuler/clj_test.clj
@@ -4,4 +4,5 @@
 
 (deftest problem1-test-1
   (testing "Problem 1"
-    (is (= (problem1 10) 23))))
+    (is (= (problem1 10) (+ 3 5 6 9)))
+    (is (= (problem1 15) 45))))
