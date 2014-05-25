bc9a368a01649d73d366804111b2b5cdd95b954c
António Nuno Monteiro <anmonteiro@gmail.com>
2014-05-25 19:13:54 +0100

    solve problem 2

--- src/projecteuler/problem2.clj
+++ src/projecteuler/problem2.clj
@@ -1,6 +1,19 @@
 (ns projecteuler.problem2)
 
-(defn problem2
+(defn fib
 	""
 	[x]
-	x)
+	(if (= x 0)
+		1
+		(if (= x 1)
+			1
+			(+ (fib (- x 1)) (fib (- x 2))))))
+
+(defn problem2
+	""
+	[limit]
+	(let [lstFib (vec(for [x (range)
+						:let [fibx (fib x)]
+						:while (<= fibx limit)
+						:when (even? fibx)] fibx))]
+		(reduce + lstFib)))
--- test/projecteuler/clj_test.clj
+++ test/projecteuler/clj_test.clj
@@ -9,6 +9,21 @@
     (is (= (problem1 10) (+ 3 5 6 9)))
     (is (= (problem1 15) 45))))
 
+
 (deftest problem2-test-1
+	(testing "Problem 2 Fibonacci sequence"
+		(is (= (fib 0) 1))
+		(is (= (fib 1) 1))
+		(is (= (fib 2) 2))
+		(is (= (fib 3) 3))
+		(is (= (fib 5) 8))
+		(is (= (fib 10) 89))))
+
+
+(deftest problem2-test-2
   (testing "Problem 2"
-    (is (= (problem2 10) 10))))
+    (is (= (problem2 89) (+ 2 8 34)))
+    (is (= (problem2 13) (+ 2 8)))
+    (is (= (problem2 8) (+ 2 8)))
+    (is (= (problem2 21) (+ 2 8)))))
+
