dd9728635b99e70a7cb784e04647b2e4861f53dd
António Nuno Monteiro <anmonteiro@gmail.com>
2014-05-26 15:52:47 +0100

    solve problem 3 properly

--- src/projecteuler/problem3.clj
+++ src/projecteuler/problem3.clj
@@ -1,20 +1,19 @@
-(ns projecteuler.problem3)
-
-(defn is-prime
-	""
-	[x]
-	)
+(ns projecteuler.problem3
+	(:require [clojure.set]))
 
 (defn prime-factors
 	"Find all prime factors of x"
 	[x]
-	(set
-		(loop [i 2 result ()]
-			(if (>= i x)
-				result
-				(recur (+ i 1) (if (and (= (mod x i) 0) (= (not-empty (prime-factors i)) nil)) (conj result i) result))))))
+	(if (<= x 1)
+		#{}
+		(loop [div 2]
+			(if (= (mod x div) 0)
+				(clojure.set/union #{div} (prime-factors (/ x div)))
+				(recur (+ div 1))))))
 
 (defn problem3
 	"Problem 3 solution"
 	[x]
-	(last (prime-factors x)))
+	(last (apply sorted-set (prime-factors x))))
+
+(problem3 600851475143)
